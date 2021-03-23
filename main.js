492c5fffb4fdc8a49d329f862603b02f0a84bd37
arteck <6681528+arteck@users.noreply.github.com>
2021-03-23 08:26:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -164,14 +164,14 @@ class judoisoftControll extends utils.Adapter {
 
         try {
             // check data
-            let conResult = await axios.get(baseUrl + "?token=" + _tokenData + "&group=register&command=get%20device%20data");
+            let conResult = await axios.get(baseUrl + "?token=" + _tokenData + "&group=register&command=get%20device%20data", { httpsAgent: agent });
 
 //            this.log.debug("get Data from" + JSON.(conResult));
 
             if (conResult.data[0].status == 'error') {
                 this.log.info("reconnect " + Date.now());
                 _tokenData = await this.getTokenFirst();
-                conResult = await axios.get(baseUrl + "?token=" + _tokenData + "&group=register&command=get%20device%20data");
+                conResult = await axios.get(baseUrl + "?token=" + _tokenData + "&group=register&command=get%20device%20data", { httpsAgent: agent });
             }
 
             let result;
