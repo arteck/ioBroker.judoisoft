f0d9d582746e9f020307a001a026570aa021516a
arteck <6681528+arteck@users.noreply.github.com>
2021-11-19 20:40:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -135,7 +135,7 @@ class judoisoftControll extends utils.Adapter {
              const instDat = await axios.get(baseUrl + "contract&command=init%20date&msgnumber=1&token=" + _tokenData, { httpsAgent: agent });   //InstallationDate
          //    const inst = this.timeConverter(Number(instDat.data.data));
              const inst = new Date(Number(instDat.data.data) * 1000);
-             this.setState("InstallationDate", inst, true);       
+             this.setState("InstallationDate", JSON.stringify(inst), true);       
          } catch (err) {
             this.log.error('InstallationDate ERROR ');
          }       
@@ -144,7 +144,7 @@ class judoisoftControll extends utils.Adapter {
              const servDat = await axios.get(baseUrl + "contract&command=service%20date&msgnumber=1&token=" + _tokenData, { httpsAgent: agent });   //ServiceDate
           //   const serv = this.timeConverter(Number(servDat.data.data));
              const serv = new Date(Number(servDat.data.data) * 1000);
-             this.setState("ServiceDate", serv, true);                       
+             this.setState("ServiceDate", JSON.stringify(serv), true);                       
          } catch (err) {
             this.log.error('ServiceDate ERROR ');
          }                             
