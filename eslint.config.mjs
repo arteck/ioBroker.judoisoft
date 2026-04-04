ed5a2dacac2d67da67c5b91b93794d6892076ea3
Simon Fischer <simi.fischa@gmail.com>
2026-04-04 23:28:16 +0200

    migrate to iobroker linting config. closes #114

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,50 +1,37 @@
-const globals = require('globals');
-const js = require('@eslint/js');
+import config from '@iobroker/eslint-config';
 
-const { FlatCompat } = require('@eslint/eslintrc');
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
-});
-
-module.exports = [
+export default [
+    ...config,
     {
         ignores: ['.dev-server/**'],
     },
-    ...compat.extends('eslint:recommended', 'plugin:prettier/recommended'),
     {
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.mocha,
-            },
-
-            ecmaVersion: 2022,
-            sourceType: 'commonjs',
-
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-        },
-
         rules: {
             indent: ['error', 4, { SwitchCase: 1 }],
-
             'prettier/prettier': ['off', { endOfLine: 'auto' }],
             'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
             'no-useless-escape': 'off',
             'no-console': 'off',
             'no-var': 'error',
             'no-trailing-spaces': 'error',
             'prefer-const': 'warn',
+            'prefer-template': 'off',
             'no-case-declarations': 'warn',
             'no-prototype-builtins': 'warn',
             'no-global-assign': 'warn',
-
+            curly: 'off',
+            'quote-props': 'off',
+            'no-else-return': 'off',
+            'jsdoc/require-jsdoc': 'off',
+            'jsdoc/check-tag-names': 'off',
+            'jsdoc/tag-lines': 'off',
+            'jsdoc/require-param-description': 'off',
+            'jsdoc/require-returns-description': 'off',
+            'jsdoc/no-defaults': 'off',
+            'no-undef': 'off',
+            'brace-style': 'off',
+            '@typescript-eslint/ban-ts-comment': 'off',
             quotes: [
                 'error',
                 'single',
@@ -53,6 +40,12 @@ module.exports = [
                     allowTemplateLiterals: true,
                 },
             ],
+        },
+    },
+    {
+        files: ['**/*.d.ts'],
+        rules: {
+            quotes: 'off',
         },
     },
 ];
